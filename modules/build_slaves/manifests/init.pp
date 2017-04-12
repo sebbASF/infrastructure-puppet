de2ec8de84a43d4feab7e11a948099ce9704d37e
Daniel Takamori <pono@apache.org>
2017-04-12 16:28:29 -0600

    remove users provisioned on Y! nodes

--- modules/build_slaves/manifests/init.pp
+++ modules/build_slaves/manifests/init.pp
@@ -2,10 +2,19 @@
 
 class build_slaves (
   $distro_packages  = [],
+  $old_users =['eli', 'jlowe', 'rvs', 'tedyu', 'tmary', 'tucu', 'cos', 'evans', 'jfarrell', 'michim', 'shv', 'tgravas', 'todd', 'wang'],
   ) {
 
   class { "build_slaves::install::${::asfosname}::${::asfosrelease}":
   }
+
+  define build_slaves::remove_users ($user = $title) {
+    exec { 'remove_old_users':
+      command => 'userdel -f ${user} && rm -rf /home/${user}',
+      onlyif  => 'grep -c ${user} /etc/passwd'
+    }
+  }
+  build_slaves::remove_users { $old_users: }
 
   package {
     $distro_packages:
