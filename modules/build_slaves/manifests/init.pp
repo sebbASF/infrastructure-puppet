3aa58e3ba1c91865202f6222d513705b10752b78
Daniel Takamori <pono@apache.org>
2017-04-12 19:14:26 -0600

    turn fanciness into a stupid hack

--- modules/build_slaves/manifests/init.pp
+++ modules/build_slaves/manifests/init.pp
@@ -2,22 +2,22 @@
 
 class build_slaves (
   $distro_packages  = [],
-  $old_users =['eli', 'jlowe', 'rvs', 'tedyu', 'tmary', 'tucu', 'cos', 'evans', 'jfarrell', 'michim', 'shv', 'tgravas', 'todd', 'wang'],
   ) {
-
-  require build_slaves
 
   class { "build_slaves::install::${::asfosname}::${::asfosrelease}":
   }
 
-  define build_slaves::remove_users ($user = $title) {
-    exec { "remove_${user}":
-      command => 'userdel -f ${user} && rm -rf /home/${user}',
-      provider => shell,
-      onlyif  => 'grep -c ${user} /etc/passwd'
-    }
-  }
-  build_slaves::remove_users { $old_users: }
+	# this is a stupid hack to fix a stupid problem
+	# this hard coded list of users was added to the Y! image templates
+	# so this guarantees these users are not on the system
+	# the onlyif is because if one user is on, then they all probably are
+	# this will error in puppet (but run correctly) if a non-empty subset of
+	#     users is not there
+	exec { "remove-old-accounts":
+		command => 'for i in eli jlowe rvs tedyu tmary tucu cos evans jfarrell michim shv tgravas todd wang; do /bin/grep -c $i /etc/passwd && /usr/sbin/userdel -f $i && /bin/rm -rf /home/$i; done',
+		provider => shell,
+		onlyif => /bin/grep -c eli /etc/passwd,
+	}
 
   package {
     $distro_packages:
