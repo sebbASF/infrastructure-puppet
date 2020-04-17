ebb8789e53158fcf68b493d8f7912f33809f6403
Sebb <sebb@apache.org>
2020-04-17 17:04:29 +0100

    Add a timeout (10 mins)

--- modules/mail_archives/files/scripts/setlock.pl
+++ modules/mail_archives/files/scripts/setlock.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-#  setlock.pl
-#  takes no options
+#  setlock.pl Execute command while holding exclusive lock
+#  Defaults to waiting 10 minutes for the lock
 #  Usage:  setlock.pl /path/to/lockfile $command @args
 
 =head1 NOTICE TO MAINTAINERS
@@ -14,12 +14,22 @@ use strict;
 use warnings FATAL => 'all';
 use Fcntl ":flock";
 
+my $timeout = $ENV{TIMEOUT} || 600; # 10 minutes
+
 my ($lockfile, $command, @args) = @ARGV;
 
 die "Usage: $0 /path/to/lockfile command args"
     unless $lockfile and $command;
 
 open my $fh, "+>>", $lockfile or die "Can't open lockfile $lockfile: $!";
-flock $fh, LOCK_EX or die "Can't get exclusive lock on $lockfile: $!";
+eval {
+    local $SIG{ALRM} = sub { die "Timeout waiting for lock ($timeout)" };
+    alarm $timeout;
+	flock $fh, LOCK_EX or die "Can't get exclusive lock on $lockfile: $!";
+    alarm 0;
+};
+die "$@\n" if ($@);
+
+# Lock granted, execute the command
 exit system $command, @args;
 
