ecd88f6636b0b7b2a14d9b281a8d102b1aba7675
Sebb <sebb@apache.org>
2020-04-17 17:23:20 +0100

    Make timeout optional

--- modules/mail_archives/files/scripts/setlock.pl
+++ modules/mail_archives/files/scripts/setlock.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
 
-#  setlock.pl Execute command while holding exclusive lock
-#  Defaults to waiting 10 minutes for the lock
-#  Usage:  setlock.pl /path/to/lockfile $command @args
+#  setlock.pl Execute command while holding exclusive lock, with optional wait time
+#  Usage:  setlock.pl [-t secs] /path/to/lockfile $command @args
 
 =head1 NOTICE TO MAINTAINERS
 
@@ -14,7 +13,11 @@ use strict;
 use warnings FATAL => 'all';
 use Fcntl ":flock";
 
-my $timeout = $ENV{TIMEOUT} || 600; # 10 minutes
+my $timeout = 0;
+if ($ARGV[0] eq '-t') {
+    shift;
+    $timeout=(shift) + 0;
+};
 
 my ($lockfile, $command, @args) = @ARGV;
 
@@ -22,13 +25,17 @@ die "Usage: $0 /path/to/lockfile command args"
     unless $lockfile and $command;
 
 open my $fh, "+>>", $lockfile or die "Can't open lockfile $lockfile: $!";
-eval {
-    local $SIG{ALRM} = sub { die "Timeout waiting for lock ($timeout)" };
-    alarm $timeout;
+if ($timeout > 0) {
+    eval {
+        local $SIG{ALRM} = sub { die "Timeout waiting for lock ($timeout)" };
+        alarm $timeout;
+        flock $fh, LOCK_EX or die "Can't get exclusive lock on $lockfile: $!";
+        alarm 0;
+    };
+    die "$@\n" if ($@);
+} else {
 	flock $fh, LOCK_EX or die "Can't get exclusive lock on $lockfile: $!";
-    alarm 0;
-};
-die "$@\n" if ($@);
+}
 
 # Lock granted, execute the command
 exit system $command, @args;
